101559270fe68b4eb022b5d311982192f9378995
Mahnoor Cheema <cheema.mahnoor@gmail.com>
2021-06-20 15:19:27 -0400

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "hello-world" is now active!');
+	let disposable = vscode.commands.registerCommand('hello-world.helloWorld', async () => {
+		vscode.window.showInformationMessage('Hello VSCODE!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
